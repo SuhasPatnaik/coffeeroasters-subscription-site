3cd9780c85f55d655c136cbdb6dc8e1b3238486f
Suhas Patnaik <suhaspatnaik111@gmail.com>
2025-02-28 13:51:10 +0530

    Custom color and font palette configured

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import LandingPage from "./components/page/LandingPage";
 import AboutUsPage from "./components/page/AboutUsPage";
 import SubscriptionPage from "./components/page/SubscriptionPage";
+import PageNotFound from "./components/page/PageNotFound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/subscribe" element={<SubscriptionPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
