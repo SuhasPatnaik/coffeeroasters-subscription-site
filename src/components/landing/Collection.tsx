9226dd129b480bb3bc724cd9dcb1ee8bc3565d37
Suhas Patnaik <suhaspatnaik111@gmail.com>
2025-03-01 12:26:15 +0530

    Created a list of collections for clean render logic

--- src/components/landing/Collection.tsx
+++ src/components/landing/Collection.tsx
@@ -1,9 +1,28 @@
+import { collectionList } from "../../utils/constants";
+
 export default function Collection() {
   return (
     <div className="grid place-items-center text-center">
       <h1 className="font-heading text-h2 bg-linear-to-b from-neutral-900 to-neutral-200 bg-clip-text text-transparent">
         our collection
       </h1>
+      {collectionList.map((collection, index) => {
+        return (
+          <div className="flex flex-col gap-4 mt-12" key={index}>
+            <img
+              src={collection.imgUrl}
+              alt="Coffeeroasters collection image"
+              className="h-32 self-center"
+            />
+            <h2 className="text-h4 font-heading text-neutral-900">
+              {collection.name}
+            </h2>
+            <p className="text-neutral-900 text-balance">
+              {collection.description}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,26 @@
+export const collectionList = [
+  {
+    name: "Gran Espresso",
+    description:
+      "Light and flavorful blend with cocoa and black pepper for an intense experience",
+    imgUrl: "/images/home/desktop/image-gran-espresso.png",
+  },
+  {
+    name: "Planalto",
+    description:
+      "Brazillian dark roast with rich and velvety body, and hints of fruits and nuts",
+    imgUrl: "/images/home/desktop/image-planalto.png",
+  },
+  {
+    name: "Piccollo",
+    description:
+      "Mild and smooth blend featuring notes of toasted almond and dried cherry",
+    imgUrl: "/images/home/desktop/image-piccollo.png",
+  },
+  {
+    name: "Danche",
+    description:
+      "Ethiopian hand-harvested blend densely packed with vibrant fruit notes",
+    imgUrl: "/images/home/desktop/image-danche.png",
+  },
+];
