c83e4f927524116c4c0d273cca22d53918fd6530
Suhas Patnaik <suhaspatnaik111@gmail.com>
2025-03-03 00:39:21 +0530

    Better technique to vertically offset quality image

--- src/components/about/Quality.tsx
+++ src/components/about/Quality.tsx
@@ -1,12 +1,14 @@
 export default function Quality() {
   return (
-    <div className="bg-neutral-900 px-8 text-center mt-30 rounded-xl">
+    <div className="bg-neutral-900 text-center mt-24 rounded-xl px-16 relative">
       <img
         src="/images/about/mobile/image-quality.jpg"
         alt="A white cup of coffee with aesthetic latte art"
-        className="mt-[-27.5%] mb-[4rem] rounded-xl"
+        className="absolute rounded-xl w-[80%] left-1/2 -translate-x-1/2 -translate-y-1/2"
       />
-      <h1 className="font-heading text-h3 mb-8">Uncompromising Quality</h1>
+      <h1 className="font-heading text-h3 mb-8 mt-36">
+        Uncompromising Quality
+      </h1>
       <p className="opacity-80 mb-16">
         Although we work with growers who pay close attention to all stages of
         harvest and processing, we employ, on our end, a rigorous quality
