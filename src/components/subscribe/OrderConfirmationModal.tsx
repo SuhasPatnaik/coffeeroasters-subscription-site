42e4d7c41884ec2565c378e83fe2a6e01f79af84
Suhas Patnaik <suhaspatnaik111@gmail.com>
2025-03-07 16:24:46 +0530

    Using react/font-awesome library for close icon

--- src/components/subscribe/OrderConfirmationModal.tsx
+++ src/components/subscribe/OrderConfirmationModal.tsx
@@ -1,18 +1,29 @@
 import Button from "../shared/Button";
 import OrderSummaryText from "./OrderSummaryText";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
 export default function OrderConfirmationModal({
   selectedPreferences,
   onClose,
 }) {
+  const handleCheckout = () => {};
+
   return (
     <>
-      <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
-        <div className="bg-white rounded-lg w-[90vw] h-[90vh] relative">
-          <h2 className="font-heading text-h3 mb-4 bg-neutral-900 py-6 pl-6 rounded-t-lg">
-            Order Summary
-          </h2>
-          <div className="flex flex-col px-8 pt-10 pb-4 gap-6">
+      <div className="fixed inset-0 bg-black/90 flex items-center justify-center z-50">
+        <div className="bg-white rounded-lg w-[90vw] h-[80vh] relative">
+          <div className="flex justify-between bg-neutral-900 p-6">
+            <h2 className="font-heading text-h3 rounded-t-lg">Order Summary</h2>
+            <button className="cursor-pointer" onClick={onClose}>
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                style={{ fontSize: "1.75rem" }}
+              />{" "}
+            </button>
+          </div>
+          <div className="flex flex-col px-8 pt-10 pb-4 gap-8">
             <p className="font-heading text-h4/loose text-neutral-500">
               <OrderSummaryText selectedPreferences={selectedPreferences} />
             </p>
@@ -21,7 +32,7 @@ export default function OrderConfirmationModal({
               selection if something is off. Subscription discount codes can
               also be redeemed at the checkout.{" "}
             </p>
-            <Button onClick={onClose} className="w-full">
+            <Button onClick={() => handleCheckout} className="w-full">
               Checkout - ${} / mo
             </Button>
           </div>
