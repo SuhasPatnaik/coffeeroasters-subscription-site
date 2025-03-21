9a52c6984f3208a063941294deba5ef9e33d7f91
SuhasPatnaik <suhaspatnaik111@gmail.com>
2025-03-22 00:06:07 +0530

    HQ + Footer: Desktop responsive

--- src/components/about/Headquarters.tsx
+++ src/components/about/Headquarters.tsx
@@ -2,31 +2,36 @@ import { hqLocations } from "../../utils/constants";
 
 export default function Headquarters() {
   return (
-    <section className="grid place-content-center place-items-center">
+    <section className="flex flex-col items-center w-full md:items-start md:px-12">
       <p className="font-heading text-h4 text-neutral-500">Our Headquarters</p>
-      {hqLocations.map((location, index) => {
-        return (
-          <div key={index} className="text-center pb-8">
-            <img
-              src={location.imgUrl}
-              alt={`${location.name} country outline respresentation`}
-              className="inline-block my-10"
-            />
-            <h3 className="font-heading text-h3 text-neutral-900 mb-6">
-              {location.name}
-            </h3>
-            <div className="flex flex-col gap-1">
-              {location.address.split(",").map((addressSplit, index) => {
-                return (
-                  <p key={index} className="text-neutral-900">
-                    {addressSplit}
-                  </p>
-                );
-              })}
+      <div className="flex flex-col w-full md:justify-between md:flex-row">
+        {hqLocations.map((location, index) => {
+          return (
+            <div
+              key={index}
+              className="text-center md:text-left flex-1 min-w-[30%]"
+            >
+              <img
+                src={location.imgUrl}
+                alt={`${location.name} country outline representation`}
+                className="inline-block my-10"
+              />
+              <h3 className="font-heading text-h3 text-neutral-900 mb-6">
+                {location.name}
+              </h3>
+              <div className="flex flex-col gap-1">
+                {location.address.split(",").map((addressSplit, index) => {
+                  return (
+                    <p key={index} className="text-neutral-900">
+                      {addressSplit}
+                    </p>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </section>
   );
 }
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,29 +2,23 @@ import { Link } from "react-router";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto bg-neutral-900 text-center py-12">
+    <footer className="mt-auto bg-neutral-900 text-center py-12 md:mt-24 md:flex md:justify-around md:items-center">
       <img
         src="/images/shared/desktop/logo-light.svg"
         alt="Coffee Roasters brand logo"
-        className="place-self-center pb-12"
+        className="place-self-center pb-12 md:pb-0"
       />
-      <div className="flex flex-col gap-4 text-neutral-500 text-nav pb-12">
-        <button className="cursor-pointer hover:text-neutral-200">
-          <Link to="/" className="uppercase">
-            home
-          </Link>
-        </button>
-        <button className="cursor-pointer hover:text-neutral-200">
-          <Link to="/about" className="uppercase">
-            about us
-          </Link>
-        </button>
-        <button className="cursor-pointer hover:text-neutral-200">
-          <Link to="/subscribe" className="uppercase">
-            create your plan
-          </Link>
-        </button>
-      </div>
+      <nav className="flex flex-col gap-4 text-neutral-500 text-nav pb-12 md:flex-row md:pb-0 md:items-center">
+        <Link to="/" className="uppercase hover:text-neutral-200">
+          home
+        </Link>
+        <Link to="/about" className="uppercase hover:text-neutral-200">
+          about us
+        </Link>
+        <Link to="/subscribe" className="uppercase hover:text-neutral-200">
+          create your plan
+        </Link>
+      </nav>
       <div className="flex gap-6 justify-center">
         <img
           src="/images/shared/desktop/icon-facebook.svg"
